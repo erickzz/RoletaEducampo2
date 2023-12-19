ccfdff57f0892c0586b427dca2636b3354a7be54
erickzz <erick.reis2@gmail.com>
2023-12-19 12:04:58 -0300

    Update perguntas.js

--- src/perguntas.js
+++ src/perguntas.js
@@ -1,460 +1,238 @@
 export const Perguntas = [
   {
     tema: 'Produção',
-    pergunta: 'Qual é o principal estado produtor de café no Brasil?  ',
+    pergunta: 'Qual é o principal estado produtor de café no Brasil?',
     alternativas: [
-      {
-        texto: 'Minas Gerais',
-        valor: true,
-      },
-      {
-        texto: 'São Paulo',
-        valor: false,
-      },
-      {
-        texto: 'Bahia',
-        valor: false,
-      },
-    ],
-  },
-  //convert the rest of the questions to this format
-  {
-    tema: 'Produção',
-    pergunta: 'Qual é a espécie de café mais cultivada no Brasil?  ',
-    alternativas: [
-      {
-        texto: 'Coffea arabica',
-        valor: true,
-      },
-      {
-        texto: 'Coffea canephora (conilon)',
-        valor: false,
-      },
-      {
-        texto: 'Coffea liberica',
-        valor: false,
-      },
+      { texto: 'Minas Gerais', valor: true },
+      { texto: 'São Paulo', valor: false },
+      { texto: 'Bahia', valor: false },
     ],
   },
   {
     tema: 'Produção',
-    pergunta: 'Qual é o principal país importador de café brasileiro?  ',
+    pergunta: 'Qual é a espécie de café mais cultivada no Brasil?',
     alternativas: [
-      {
-        texto: 'Alemanha',
-        valor: false,
-      },
-      {
-        texto: 'Estados Unidos',
-        valor: false,
-      },
-      {
-        texto: 'Itália',
-        valor: true,
-      },
+      { texto: 'Coffea arabica', valor: true },
+      { texto: 'Coffea canephora (conilon)', valor: false },
+      { texto: 'Coffea liberica', valor: false },
+    ],
+  },
+  {
+    tema: 'Produção',
+    pergunta: 'Qual é o principal país importador de café brasileiro?',
+    alternativas: [
+      { texto: 'Estados Unidos', valor: true },
+      { texto: 'Alemanha', valor: false },
+      { texto: 'Itália', valor: false },
     ],
   },
   {
     tema: 'Produção',
     pergunta:
-      'Qual é a época do ano em que ocorre a colheita do café no Brasil?  ',
+      'Qual é a época do ano em que ocorre a colheita do café no Brasil?',
     alternativas: [
-      {
-        texto: 'Inverno',
-        valor: false,
-      },
-      {
-        texto: 'Primavera',
-        valor: false,
-      },
-      {
-        texto: 'Verão',
-        valor: true,
-      },
-    ],
-  },
-  {
-    tema: 'Produção',
-    pergunta: 'Qual é a porcentagem média de água no café verde?  ',
-    alternativas: [
-      {
-        texto: 'Cerca de 11%',
-        valor: false,
-      },
-      {
-        texto: 'Cerca de 25%',
-        valor: false,
-      },
-      {
-        texto: 'Cerca de 5%',
-        valor: true,
-      },
+      { texto: 'Inverno', valor: true },
+      { texto: 'Primavera', valor: false },
+      { texto: 'Verão', valor: false },
     ],
   },
   {
     tema: 'Produção',
     pergunta:
-      'Qual é o processo utilizado para secar os grãos de café após a colheita?  ',
+      'Qual o teor de umidade ideal para o armazenamento do café após a colheita?',
     alternativas: [
-      {
-        texto: 'Secagem natural ao sol',
-        valor: true,
-      },
-      {
-        texto: 'Secagem por máquina',
-        valor: false,
-      },
-      {
-        texto: 'Secagem em estufa',
-        valor: false,
-      },
-    ],
-  },
-  {
-    tema: 'Produção',
-    pergunta: 'Qual é a classificação do café com grãos de tamanho médio?  ',
-    alternativas: [
-      {
-        texto: 'Café tradicional',
-        valor: true,
-      },
-      {
-        texto: 'Café gourmet',
-        valor: false,
-      },
-      {
-        texto: 'Café superior',
-        valor: false,
-      },
+      { texto: 'Cerca de 11%', valor: true },
+      { texto: 'Cerca de 25%', valor: false },
+      { texto: 'Cerca de 5%', valor: false },
     ],
   },
   {
     tema: 'Produção',
     pergunta:
-      'Qual é o termo utilizado para descrever a degustação profissional de café?  ',
+      'Qual é o nome do processo de secagem do café, em que os grãos passam por um lavador separando o café bóia do café cereja e verde?',
     alternativas: [
-      {
-        texto: 'Cupping',
-        valor: true,
-      },
-      {
-        texto: 'Brewing',
-        valor: false,
-      },
-      {
-        texto: 'Roasting',
-        valor: false,
-      },
+      { texto: 'Secagem via seca', valor: true },
+      { texto: 'Secagem via úmida', valor: false },
+      { texto: 'Secagem por secadores', valor: false },
     ],
   },
   {
     tema: 'Produção',
     pergunta:
-      'Qual é o método de processamento de café que utiliza água para remover a polpa dos grãos?  ',
+      'Qual é o tipo de café em que há formação de blend (mistura de café arábica com café conilon)?',
     alternativas: [
-      {
-        texto: 'Via úmida',
-        valor: true,
-      },
-      {
-        texto: 'Via seca',
-        valor: false,
-      },
-      {
-        texto: 'Via semiúmida',
-        valor: false,
-      },
+      { texto: 'Café tradicional', valor: false },
+      { texto: 'Café gourmet', valor: false },
+      { texto: 'Café Superior', valor: false },
     ],
   },
   {
     tema: 'Produção',
-    pergunta: 'Qual é o principal tipo de café produzido no Brasil?  ',
+    pergunta:
+      'Qual é o termo utilizado para descrever a degustação profissional de café?',
     alternativas: [
-      {
-        texto: 'Café arábica',
-        valor: true,
-      },
-      {
-        texto: 'Café robusta',
-        valor: false,
-      },
-      {
-        texto: 'Café especial',
-        valor: false,
-      },
+      { texto: 'Cupping', valor: true },
+      { texto: 'Brewing', valor: false },
+      { texto: 'Roasting', valor: false },
     ],
   },
   {
-    tema: 'Qualidade',
-    pergunta: 'Qual é a quantidade média de cafeína em uma xícara de café?  ',
+    tema: 'Produção',
+    pergunta:
+      'Qual é o método de processamento de café que utiliza água para remover a polpa dos grãos?',
     alternativas: [
-      {
-        texto: 'Cerca de 95 mg',
-        valor: true,
-      },
-      {
-        texto: 'Cerca de 50 mg',
-        valor: false,
-      },
-      {
-        texto: 'Cerca de 150 mg',
-        valor: false,
-      },
-    ],
-  },
-  //convert the rest of the questions to this format
-  {
-    tema: 'Qualidade',
-    pergunta: 'Qual é o país que mais consome café per capita no mundo?  ',
-    alternativas: [
-      {
-        texto: 'Finlândia',
-        valor: true,
-      },
-      {
-        texto: 'Brasil',
-        valor: false,
-      },
-      {
-        texto: 'Itália',
-        valor: false,
-      },
+      { texto: 'Via úmida', valor: true },
+      { texto: 'Via seca', valor: false },
+      { texto: 'Via semiúmida', valor: false },
     ],
   },
   {
-    tema: 'Qualidade',
-    pergunta: 'Qual é a porcentagem média de óleos essenciais no café?  ',
+    tema: 'Produção',
+    pergunta: 'Qual é o principal tipo de café produzido no Brasil?',
     alternativas: [
-      {
-        texto: 'Cerca de 1%',
-        valor: false,
-      },
-      {
-        texto: 'Cerca de 5%',
-        valor: false,
-      },
-      {
-        texto: 'Cerca de 10%',
-        valor: true,
-      },
-    ],
-  },
-  {
-    tema: 'Qualidade',
-    pergunta: 'Qual é o principal fator que influencia o sabor do café?  ',
-    alternativas: [
-      {
-        texto: 'Torra dos grãos',
-        valor: false,
-      },
-      {
-        texto: 'Origem dos grãos',
-        valor: false,
-      },
-      {
-        texto: 'Método de preparo',
-        valor: true,
-      },
-    ],
-  },
-  {
-    tema: 'Qualidade',
-    pergunta: 'Qual é o processo utilizado para retirar a cafeína do café?  ',
-    alternativas: [
-      {
-        texto: 'Descafeinação',
-        valor: true,
-      },
-      {
-        texto: 'Despolpamento',
-        valor: false,
-      },
-      {
-        texto: 'Fermentação',
-        valor: false,
-      },
-    ],
-  },
-  {
-    tema: 'Qualidade',
-    pergunta: 'Qual é o principal método de preparo de café no Brasil?  ',
-    alternativas: [
-      {
-        texto: 'Café coado',
-        valor: true,
-      },
-      {
-        texto: 'Café expresso',
-        valor: false,
-      },
-      {
-        texto: 'Café de filtro',
-        valor: false,
-      },
-    ],
-  },
-  //convert the rest of the questions to this format
-  {
-    tema: 'Qualidade',
-    pergunta: 'Qual é a classificação do café que possui zero defeitos?  ',
-    alternativas: [
-      {
-        texto: 'Café especial',
-        valor: true,
-      },
-      {
-        texto: 'Café gourmet',
-        valor: false,
-      },
-      {
-        texto: 'Café tradicional',
-        valor: false,
-      },
+      { texto: 'Café arábica', valor: true },
+      { texto: 'Café robusta', valor: false },
+      { texto: 'Café especial', valor: false },
     ],
   },
   {
     tema: 'Qualidade',
     pergunta:
-      'Qual é a temperatura ideal da água para continuar com o processo de preparo do café espresso?  ',
+      'Qual teor médio de cafeína encontrado no café arábica e no café conilon, respectivamente?',
     alternativas: [
-      {
-        texto: '88°C - 92°C',
-        valor: true,
-      },
-      {
-        texto: '70°C - 75°C',
-        valor: false,
-      },
-      {
-        texto: '100°C',
-        valor: false,
-      },
+      { texto: '1,2% e 2,2%', valor: true },
+      { texto: '2% e 5%', valor: false },
+      { texto: '0,5% e 4%', valor: false },
     ],
   },
   {
     tema: 'Qualidade',
-    pergunta: 'Qual é o termo utilizado para descrever o aroma do café?  ',
+    pergunta: 'Qual é o país que mais consome café per capita no mundo?',
     alternativas: [
-      {
-        texto: 'Fragrância',
-        valor: true,
-      },
-      {
-        texto: 'Corpo',
-        valor: false,
-      },
-      {
-        texto: 'Acidez',
-        valor: false,
-      },
+      { texto: 'Finlândia', valor: true },
+      { texto: 'Brasil', valor: false },
+      { texto: 'Itália', valor: false },
     ],
   },
   {
     tema: 'Qualidade',
     pergunta:
-      'Qual é a média de xícaras de café consumidas por dia pelos brasileiros?  ',
+      'Qual é a porcentagem média de óleos essenciais no café arábica e conilon respectivamente?',
     alternativas: [
-      {
-        texto: '3 xícaras',
-        valor: true,
-      },
-      {
-        texto: '1 xícara',
-        valor: false,
-      },
-      {
-        texto: '5 xícaras',
-        valor: false,
-      },
+      { texto: '10% - 17% e 8% - 10%', valor: true },
+      { texto: '5% - 10% e 12% - 14%', valor: false },
+      { texto: '12% - 14% e 5% - 10%', valor: false },
+    ],
+  },
+  {
+    tema: 'Qualidade',
+    pergunta: 'Qual é o principal fator que influencia o sabor do café?',
+    alternativas: [
+      { texto: 'Torra dos grãos', valor: true },
+      { texto: 'Origem dos grãos', valor: false },
+      { texto: 'Método de preparo', valor: false },
+    ],
+  },
+  {
+    tema: 'Qualidade',
+    pergunta: 'Qual é o processo utilizado para retirar a cafeína do café?',
+    alternativas: [
+      { texto: 'Descafeinação', valor: true },
+      { texto: 'Despolpamento', valor: false },
+      { texto: 'Fermentação', valor: false },
+    ],
+  },
+  {
+    tema: 'Qualidade',
+    pergunta: 'Qual é o principal método de preparo de café no Brasil?',
+    alternativas: [
+      { texto: 'Café coado', valor: true },
+      { texto: 'Café expresso', valor: false },
+      { texto: 'Café de filtro', valor: false },
+    ],
+  },
+  {
+    tema: 'Qualidade',
+    pergunta: 'Qual é a classificação do café que possui zero defeitos?',
+    alternativas: [
+      { texto: 'Café especial', valor: true },
+      { texto: 'Café gourmet', valor: false },
+      { texto: 'Café tradicional', valor: false },
+    ],
+  },
+  {
+    tema: 'Qualidade',
+    pergunta:
+      'Qual é a temperatura ideal da água para continuar com o processo de preparo do café espresso?',
+    alternativas: [
+      { texto: '88°C - 92°C', valor: true },
+      { texto: '70°C - 75°C', valor: false },
+      { texto: '100°C', valor: false },
+    ],
+  },
+  {
+    tema: 'Qualidade',
+    pergunta: 'Qual é o termo utilizado para descrever o aroma do café?',
+    alternativas: [
+      { texto: 'Fragrância', valor: true },
+      { texto: 'Corpo', valor: false },
+      { texto: 'Acidez', valor: false },
+    ],
+  },
+  {
+    tema: 'Qualidade',
+    pergunta:
+      'Qual é a média de xícaras de café consumidas por dia pelos brasileiros?',
+    alternativas: [
+      { texto: '3,8 xícaras', valor: true },
+      { texto: '1 xícara', valor: false },
+      { texto: '5,5 xícaras', valor: false },
     ],
   },
   {
     tema: 'Curiosidades',
-    pergunta: 'Qual é a origem do café?  ',
+    pergunta: 'Qual é a origem do café?',
     alternativas: [
-      {
-        texto: 'Etiópia',
-        valor: true,
-      },
-      {
-        texto: 'Brasil',
-        valor: false,
-      },
-      {
-        texto: 'Colômbia',
-        valor: false,
-      },
+      { texto: 'Etiópia', valor: true },
+      { texto: 'Brasil', valor: false },
+      { texto: 'Colômbia', valor: false },
     ],
   },
-  //convert the rest of the questions to this format
   {
     tema: 'Curiosidades',
-    pergunta: 'Qual é a maior feira de café do Brasil?  ',
+    pergunta: 'Qual é a maior feira de café do Brasil?',
     alternativas: [
-      {
-        texto: 'Semana Internacional do Café',
-        valor: true,
-      },
-      {
-        texto: 'ExpoCafé',
-        valor: false,
-      },
-      {
-        texto: 'Feira Nacional do Café',
-        valor: false,
-      },
+      { texto: 'Semana Internacional do Café', valor: true },
+      { texto: 'ExpoCafé', valor: false },
+      { texto: 'Feira Nacional do Café', valor: false },
     ],
   },
   {
     tema: 'Curiosidades',
     pergunta:
-      'Qual é a porcentagem média de cafés especiais produzidos no Brasil?  ',
+      'Qual é a porcentagem média de cafés especiais produzidos no Brasil?',
     alternativas: [
-      {
-        texto: 'Cerca de 10%',
-        valor: true,
-      },
-      {
-        texto: 'Cerca de 50%',
-        valor: false,
-      },
-      {
-        texto: 'Cerca de 80%',
-        valor: false,
-      },
+      { texto: 'Cerca de 20%', valor: true },
+      { texto: 'Cerca de 5%', valor: false },
+      { texto: 'Cerca de 40%', valor: false },
     ],
   },
   {
     tema: 'Curiosidades',
-    pergunta: 'Qual é o período de vida útil do café torrado e moído?  ',
+    pergunta: 'Qual é o período de vida útil do café torrado e moído?',
     alternativas: [
-      {
-        texto: '6 meses',
-        valor: true,
-      },
-      {
-        texto: '6 anos',
-        valor: false,
-      },
-      {
-        texto: '1 ano',
-        valor: false,
-      },
+      { texto: '6 meses', valor: true },
+      { texto: '6 anos', valor: false },
+      { texto: '1 ano', valor: false },
     ],
   },
   {
     tema: 'Curiosidades',
     pergunta:
-      'Qual é a altitude ideal para o cultivo de café de alta qualidade?  ',
+      'Qual é a altitude ideal para o cultivo de café de alta qualidade?',
     alternativas: [
-      {
-        texto: 'Entre 800 e 1.800 metros acima do nível do mar',
-        valor: true,
-      },
-      {
-        texto: 'Entre 100 e 500 metros acima do nível do mar',
-        valor: false,
-      },
+      { texto: 'Entre 800 e 1.800 metros acima do nível do mar', valor: true },
+      { texto: 'Entre 100 e 500 metros acima do nível do mar', valor: false },
       {
         texto: 'Entre 2.000 e 3.000 metros acima do nível do mar',
         valor: false,
@@ -464,94 +242,30 @@ export const Perguntas = [
   {
     tema: 'Curiosidades',
     pergunta:
-      'Qual é a principal variedade de café cultivada na região do Cerrado no Brasil?  ',
+      'Qual é a principal variedade de café cultivada na região do Cerrado no Brasil?',
     alternativas: [
-      {
-        texto: 'Catuaí',
-        valor: true,
-      },
-      {
-        texto: 'Bourbon',
-        valor: false,
-      },
-      {
-        texto: 'Mundo Novo',
-        valor: false,
-      },
+      { texto: 'Catuaí', valor: true },
+      { texto: 'Bourbon', valor: false },
+      { texto: 'Mundo Novo', valor: false },
     ],
   },
   {
     tema: 'Curiosidades',
     pergunta:
-      'Qual é o processo utilizado para obter grãos de café despolpados?  ',
+      'Qual é o processo utilizado para obter grãos de café despolpados?',
     alternativas: [
-      {
-        texto: 'Via úmida',
-        valor: true,
-      },
-      {
-        texto: 'Via seca',
-        valor: false,
-      },
-      {
-        texto: 'Via fermentação',
-        valor: false,
-      },
+      { texto: 'Via úmida', valor: true },
+      { texto: 'Via seca', valor: false },
+      { texto: 'Via fermentação', valor: false },
     ],
   },
   {
     tema: 'Curiosidades',
-    pergunta:
-      'Qual é o tipo de café que passa por um processo de envelhecimento?  ',
+    pergunta: 'Qual é o tipo de café mais caro do mundo?',
     alternativas: [
-      {
-        texto: 'Café guarda',
-        valor: true,
-      },
-      {
-        texto: 'Café raro',
-        valor: false,
-      },
-      {
-        texto: 'Café envelhecido',
-        valor: false,
-      },
-    ],
-  },
-  {
-    tema: 'Curiosidades',
-    pergunta: 'Qual é o tipo de café mais caro do mundo?  ',
-    alternativas: [
-      {
-        texto: 'Kopi Luwak',
-        valor: true,
-      },
-      {
-        texto: 'Blue Mountain',
-        valor: false,
-      },
-      {
-        texto: 'Black Ivory',
-        valor: false,
-      },
-    ],
-  },
-  {
-    tema: 'Curiosidades',
-    pergunta: 'Qual é o maior evento relacionado ao café do mundo?  ',
-    alternativas: [
-      {
-        texto: 'World of Coffee',
-        valor: true,
-      },
-      {
-        texto: 'SCA Expo',
-        valor: false,
-      },
-      {
-        texto: 'International Coffee Festival',
-        valor: false,
-      },
+      { texto: 'Kopi Luwak', valor: true },
+      { texto: 'Blue Mountain', valor: false },
+      { texto: 'Black Ivory', valor: false },
     ],
   },
 ];
